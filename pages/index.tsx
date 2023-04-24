622edd80eb626a69718b207edb9fce7227571487
rashmi-fluence <107472399+rashmi-fluence@users.noreply.github.com>
2023-04-24 22:33:11 +0530

    Uncommented redis stream code

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,16 +12,16 @@ export default function HomePage({items}) {
 
 export async function getServerSideProps(context) {
   await createConnection();
-    // await populateStream();
+    await populateStream();
     
-    // console.log("fetching from stream");
-    // continuousReadMessages();
-    //  console.log("End of fetchmessages---------");
+    console.log("fetching from stream");
+    continuousReadMessages();
+     console.log("End of fetchmessages---------");
 
   const res = await fetch(`http://localhost:3000/api/handler`);
-  console.log(res);
+  // console.log(res);
   const items = await res.json();
-  console.log(`Printing: ${JSON.stringify(items)}`);
+  // console.log(`Printing: ${JSON.stringify(items)}`);
   return {
     props: {
       items
